5537463a4f6c16ba1412ca5438e31d3f5df20d49
chetan kokil <chetan.kokil@gmail.com>
2016-02-04 14:39:16 -0600

    Added Some Util Classes and some extra dependencies

--- src/main/java/com/test/restclient/RestUtil.java
+++ src/main/java/com/test/restclient/RestUtil.java
@@ -1,6 +1,9 @@
 package com.test.restclient;
+
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -16,12 +19,13 @@ import java.io.InputStream;
 @Slf4j
 public class RestUtil {
 
-    public static String get(String url) {
+    public static String get(String url, String contentType) {
         HttpClient httpClient = new DefaultHttpClient();
         StringBuilder sb = new StringBuilder();
         try {
             log.debug("Url is {}", url);
             HttpGet httpGet = new HttpGet(url);
+            httpGet.setHeader(HttpHeaders.CONTENT_TYPE, contentType);
             HttpResponse response = httpClient.execute(httpGet);
 
             HttpEntity entity = response.getEntity();
@@ -64,5 +68,64 @@ public class RestUtil {
             httpClient.getConnectionManager().shutdown();
         }
         return sb.length() > 0 ? sb.toString() : null;
+    }
+
+
+    public static <T> T get(String url, String contentType, Class<T> clazz) {
+        HttpClient httpClient = new DefaultHttpClient();
+        System.out.println();
+        StringBuilder sb = new StringBuilder();
+        try {
+            log.debug("Url is {}", url);
+            HttpGet httpGet = new HttpGet(url);
+            httpGet.setHeader(HttpHeaders.CONTENT_TYPE, contentType);
+            HttpResponse response = httpClient.execute(httpGet);
+
+            HttpEntity entity = response.getEntity();
+
+            byte[] buffer = new byte[1024];
+            if (entity != null) {
+                log.debug("Entity is not null");
+                InputStream inputStream = entity.getContent();
+                try {
+                    int bytesRead = 0;
+                    BufferedInputStream bis = new BufferedInputStream(inputStream);
+
+                    while ((bytesRead = bis.read(buffer)) != -1) {
+                        String chunk = new String(buffer, 0, bytesRead);
+                        sb.append(chunk);
+                    }
+
+                } catch (IOException ioException) {
+                    // In case of an IOException the connection will be released
+                    // back to the connection manager automatically
+                    ioException.printStackTrace();
+                } catch (RuntimeException runtimeException) {
+                    // In case of an unexpected exception you may want to abort
+                    // the HTTP request in order to shut down the underlying
+                    // connection immediately.
+                    httpGet.abort();
+                    runtimeException.printStackTrace();
+                } finally {
+                    // Closing the input stream will trigger connection release
+                    try {
+                        inputStream.close();
+                    } catch (Exception ignore) {
+                    }
+                }
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            httpClient.getConnectionManager().shutdown();
+        }
+
+        if (sb.length() > 0) {
+            return new Gson().fromJson(sb.toString(), clazz);
+        } else {
+            return null;
+        }
+
     }
 }
--- src/test/java/RestUtilTest.groovy
+++ src/test/java/RestUtilTest.groovy
@@ -1,10 +1,20 @@
+import com.test.pojo.Quote
+import com.test.restclient.RestUtil
+
 /**
  * Created by chetankokil on 2/4/16.
  */
 class RestUtilTest extends groovy.util.GroovyTestCase {
     void testGet() {
-        String str = RestUtil.get("http://gturnquist-quoters.cfapps.io/api/random")
+        String str = RestUtil.get("http://gturnquist-quoters.cfapps.io/api/random", "application/json")
         println(str)
         assertNotNull(str)
+    }
+
+
+    void testGetUsingClass() {
+        Quote quote = RestUtil.get("http://gturnquist-quoters.cfapps.io/api/random", "application/json", Quote.class)
+        assertNotNull(quote)
+        println(quote)
     }
 }
